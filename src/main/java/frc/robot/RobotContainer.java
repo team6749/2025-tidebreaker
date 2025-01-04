db59df9ecd14c7ed4673984fa3d49f8d0554422d
SetheenDev <andy@pinefog.com>
2025-01-04 15:08:34 -0600

    feat(shuffleboard): add fake status variable

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,8 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -30,6 +32,10 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+
+    // Add data to Shuffleboard
+    ShuffleboardTab dashboard = Shuffleboard.getTab("Dashboard");
+    dashboard.add("Fake Robot Status", "Operational");
   }
 
   /**
