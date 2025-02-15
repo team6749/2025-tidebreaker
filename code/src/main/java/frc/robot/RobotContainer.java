02c685ce2f40c7d3e0e7c95ef42d4fbf60b935e1
RoboDoggo <150201129+robodoggoYT@users.noreply.github.com>
2025-02-14 20:21:37 -0600

    Implement path planner (#35)
    * Did in fact implement it
    
    * made test autos and tested them (theyre good)

--- code/src/main/java/frc/robot/RobotContainer.java
+++ code/src/main/java/frc/robot/RobotContainer.java
@@ -10,11 +10,20 @@ import static edu.wpi.first.units.Units.Radians;
 import static edu.wpi.first.units.Units.Volt;
 import static edu.wpi.first.units.Units.Volts;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -24,6 +33,7 @@ import frc.robot.subsystems.Elevator;
 
 @Logged
 public class RobotContainer {
+  private final SendableChooser<Command> autoChooser;
 
   SwerveDrive swerveSubsystem;
   Localization localizationSubsystem;
@@ -40,7 +50,43 @@ public class RobotContainer {
     swerveSubsystem = new SwerveDrive();
     localizationSubsystem = new Localization(swerveSubsystem);
     elevatorSubsystem = new Elevator();
+    
+    try{
+      RobotConfig config = RobotConfig.fromGUISettings();
+      // Configure AutoBuilder last
+    AutoBuilder.configure(
+      localizationSubsystem::getRobotPose, // Robot pose supplier
+      localizationSubsystem::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
+      swerveSubsystem::getChassisSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+      (speeds, feedforwards) -> swerveSubsystem.runChassisSpeeds(speeds), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
+      new PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for holonomic drive trains
+              new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+              new PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
+      ),
+      config, // The robot configuration
+      () -> {
+        // Boolean supplier that controls when the path will be mirrored for the red alliance
+        // This will flip the path being followed to the red side of the field.
+        // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+    
 
+        var alliance = DriverStation.getAlliance();
+        if (alliance.isPresent()) {
+          return alliance.get() == DriverStation.Alliance.Red;
+        }
+        return false;
+      },
+      swerveSubsystem // Reference to this subsystem to set requirements
+);
+    } catch (Exception e) {
+      // Handle exception as needed
+      e.printStackTrace();
+    }
+
+    
+  
+    autoChooser = AutoBuilder.buildAutoChooser();
+    SmartDashboard.putData("autoChooser", autoChooser);
 
     configureBindings();
     //elevatorTest();
@@ -67,9 +113,6 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    return swerveSubsystem.constantChassisSpeedsCommand(
-        new ChassisSpeeds(2, 0, 2));
-  }
-
-
+    return autoChooser.getSelected();
+    }
 }
