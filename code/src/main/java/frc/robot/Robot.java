3691f7ab9e7f5da383c25d8c92bbb8a38f8de11c
Reshiria <150201755+Reshiria@users.noreply.github.com>
2025-03-21 14:30:31 -0500

    Finished reef face to coral
    reef face to coral(looks) right

--- code/src/main/java/frc/robot/Robot.java
+++ code/src/main/java/frc/robot/Robot.java
@@ -21,6 +21,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+
 @Logged
 public class Robot extends TimedRobot {
 
