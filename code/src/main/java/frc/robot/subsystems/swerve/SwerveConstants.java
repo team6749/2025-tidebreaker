6943a38c02650d3c5bc2481e916e213af7ac3c7b
mootw <spencer.mein@gmail.com>
2025-03-27 20:01:18 -0500

    random changes (#115)
    changed (some) coral paths as the arm turned out to be 1 cm to the left. wheel diameter was increased to counteract the odometry difference. Reduced max velocity to 3.5(which it seemed to be), manually sys-id'd
    
    Co-authored-by: Reshiria <150201755+Reshiria@users.noreply.github.com>

--- code/src/main/java/frc/robot/subsystems/swerve/SwerveConstants.java
+++ code/src/main/java/frc/robot/subsystems/swerve/SwerveConstants.java
@@ -19,7 +19,7 @@ public class SwerveConstants {
     public static final SlewRateLimiter driveLimiterTheta = new SlewRateLimiter(27.5);
 
     // Max linear velocity of the module (how fast it can spin)
-    public static final LinearVelocity maxLinearVelocity = MetersPerSecond.of(4);
+    public static final LinearVelocity maxLinearVelocity = MetersPerSecond.of(3.5);
 
     // This should be based on physical properties of the the max wheel speeds in a
     // circle
@@ -28,7 +28,7 @@ public class SwerveConstants {
     public static final double angleReduction = (150.0 / 7.0);
     public static final double driveReduction = 6.82;
 
-    public static final Distance wheelDiameter = Inches.of(4);
+    public static final Distance wheelDiameter = Inches.of(4.113);
     public static final Distance wheelCircumference = wheelDiameter.times(Math.PI);
 
     // Distance between the center of wheels along the width of the robot(Y axis)
