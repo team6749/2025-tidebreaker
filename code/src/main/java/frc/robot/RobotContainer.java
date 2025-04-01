6fb9bdb3f237036a509361939127d33f3a2c20ab
Reshiria <150201755+Reshiria@users.noreply.github.com>
2025-04-01 16:15:38 -0500

    Cranked PID up to 10
    Cranked up PID, will test on robot soon.

--- code/src/main/java/frc/robot/RobotContainer.java
+++ code/src/main/java/frc/robot/RobotContainer.java
@@ -131,7 +131,7 @@ public class RobotContainer {
                                                                               // feedforwards
           new PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for
                                           // holonomic drive trains
-              new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+              new PIDConstants(10.0, 0.0, 0.0), // Translation PID constants
               new PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
           ),
           config, // The robot configuration
