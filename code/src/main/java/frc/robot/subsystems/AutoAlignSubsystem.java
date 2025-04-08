4421912e34c712f322f1a612436e638f645d5dd9
Reshiria <150201755+Reshiria@users.noreply.github.com>
2025-04-07 19:30:06 -0500

    removing brainrot

---
+++ code/src/main/java/frc/robot/subsystems/AutoAlignSubsystem.java
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class AutoAlignSubsystem extends SubsystemBase {
+  /** Creates a new AutoAlign. */
+  public AutoAlignSubsystem() {}
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public boolean vectorToABReefFace() {
+    Translation2d vector = new Translation2d(5,5);
+  }
+}
