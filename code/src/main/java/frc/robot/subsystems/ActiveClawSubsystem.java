d9dd3d7d8b6b6f674b9ec809ce106ea8dbf31780
Reshiria <150201755+Reshiria@users.noreply.github.com>
2025-04-08 19:18:15 -0500

    Added slight logging
    Tested in simulator. When the value of the limit switch is set to false, the volts go to the correct place.

--- code/src/main/java/frc/robot/subsystems/ActiveClawSubsystem.java
+++ code/src/main/java/frc/robot/subsystems/ActiveClawSubsystem.java
@@ -10,13 +10,15 @@ import static edu.wpi.first.units.Units.Volts;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-
+@Logged
 public class ActiveClawSubsystem extends SubsystemBase {
   TalonFX clawMotor;
   Voltage idleVoltage = Volts.of(0.5);
@@ -25,9 +27,9 @@ public class ActiveClawSubsystem extends SubsystemBase {
   DigitalInput clawLimitSwitch;
   /** Creates a new ActiveClaw. */
   public ActiveClawSubsystem() {
-    brakeMode(true);
     clawMotor = new TalonFX(Constants.clawMotorID); //placeholder before the claw is made
-    clawLimitSwitch = new DigitalInput(9999999);
+    brakeMode(true);
+    clawLimitSwitch = new DigitalInput(6);
   }
 
   @Override
@@ -40,16 +42,19 @@ public class ActiveClawSubsystem extends SubsystemBase {
   }
 
   public Command clawIdleState() {
-    Command command = Commands.run(() -> runVolts(Volt.of(stopOnIntake())), this);
+    Command command = Commands.runEnd(() -> runVolts(Volts.of(stopOnIntake())),() -> stop(),this);
+    command.setName("clawIdleState");
     return command;
   }
 
   public Command clawLowShoot() {
     Command command = Commands.run(() -> runVolts(shootLowVoltage), this);
+    command.setName("clawLowShoot");
     return command;
   }
   public Command clawHighShoot() {
     Command command = Commands.run(() -> runVolts(shootHighVoltage), this);
+    command.setName("clawhighShoot");
     return command;
   }
   public void brakeMode(boolean isBrakeModeOn) {
@@ -58,5 +63,8 @@ public class ActiveClawSubsystem extends SubsystemBase {
 
   public double stopOnIntake() {
     return (clawLimitSwitch.get() ? 0:1);
+  }
+  public void stop() {
+    clawMotor.stopMotor();
   }
 }
